d5474938c5ecb27bd9550b461179a02b7826144a
yymin1022 <yymin1022@gmail.com>
2024-01-11 02:52:27 +0900

    Update device.mk : Renamed for phoenix

--- device.mk
+++ device.mk
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/lge/joan
+DEVICE_PATH := device/lge/phoenix
 
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
 
 # common v30
-$(call inherit-product, device/lge/joan-common/joan.mk)
+$(call inherit-product, device/lge/phoenix-common/phoenix.mk)
 
 # Inherit proprietary blobs
-$(call inherit-product, vendor/lge/joan/joan-vendor.mk)
+$(call inherit-product, vendor/lge/phoenix/phoenix-vendor.mk)
